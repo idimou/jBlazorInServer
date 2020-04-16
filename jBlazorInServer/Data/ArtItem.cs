843c7d2ae148364675f4b253795b4bb2666a41ac
idimou <jdimou@gmail.com>
2020-04-17 00:43:38 +0200

    first gallery with dummy data

---
+++ jBlazorInServer/Data/ArtItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jBlazorInServer.Data
+{
+    public class ArtItem
+    {
+        public int ID { get; set; }
+        public string ImageURL { get; set; }
+        public string Title { get; set; }
+        public int ArtistID { get; set; }
+    }
+}
---
+++ jBlazorInServer/Data/ArtItemService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jBlazorInServer.Data
+{
+    public class ArtItemService
+    {
+        public Task<ArtItem[]> GetForecastAsync()
+        {
+
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new ArtItem
+            {
+                ID = index,
+                ImageURL= "https://ae01.alicdn.com/kf/HTB154QnXVooBKNjSZFPq6xa2XXaH.jpg",
+                Title = "Sample Title " + index.ToString(),
+                ArtistID = 1                
+            }).ToArray()) ;
+        }
+    }
+
+
+}
